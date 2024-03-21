e333679ea3b2d94809c084ea01873e981b2200aa
pubudu lasith <pubudu093@gmail.com>
2024-03-22 10:44:16 +1100

    floder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'isd',
+    version= '0.0.0',
+    author= 'pubudu',
+    author_email= 'pubudu093@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
